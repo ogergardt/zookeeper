e03d1a108326d730856997d5bf816474fb982f65
ogergardt@hotmail.com <ogergardt@hotmail.com>
2017-10-28 22:15:40 -0700

    init

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
+url="${mirror}zookeeper/zookeeper-${ZOOKEEPER_VERSION}/zookeeper-${ZOOKEEPER_VERSION}.tar.gz"
+wget -q "${url}" -O "/tmp/zookeeper-${ZOOKEEPER_VERSION}.tar.gz"
---
+++ start.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZOOKEEPER_HOME/conf/log4j.properties
+sed -i -r 's|#autopurge|autopurge|g' $ZOOKEEPER_HOME/conf/zoo.cfg
+
+$ZOOKEEPER_HOME/bin/zkServer.sh start-foreground
+
